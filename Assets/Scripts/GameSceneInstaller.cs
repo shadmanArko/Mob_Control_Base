c8b618aeed03f4ce3a485b2a940b055f0b96e8f5
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2025-07-19 23:24:36 +0200

    Extenject Setup

---
+++ Assets/Scripts/GameSceneInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "GameSceneInstaller", menuName = "Installers/GameSceneInstaller")]
+public class GameSceneInstaller : ScriptableObjectInstaller<GameSceneInstaller>
+{
+    public override void InstallBindings()
+    {
+    }
+}
